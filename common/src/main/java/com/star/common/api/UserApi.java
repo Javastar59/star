c78317c4022519a4b548defe15086cde8f85caae
caoxing <sunstar59@163.com.cn>
2019-01-06 21:37:07 +0800

    update

--- common/src/main/java/com/star/common/api/UserApi.java
+++ common/src/main/java/com/star/common/api/UserApi.java
@@ -1,4 +1,9 @@
 package com.star.common.api;
 
+import java.util.List;
+
 public interface UserApi {
+    String hello();
+
+    List list();
 }
--- service/src/main/java/com/star/service/provider/UserProvider.java
+++ service/src/main/java/com/star/service/provider/UserProvider.java
@@ -1,10 +1,13 @@
 package com.star.service.provider;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.google.common.collect.Lists;
 import com.star.common.api.UserApi;
 import com.star.service.core.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Service(version = "1.0.0")
 @Component
@@ -15,5 +18,15 @@ public class UserProvider implements UserApi {
     @Autowired
     public UserProvider(UserService userService) {
         this.userService = userService;
+    }
+
+    @Override
+    public String hello() {
+        return "hello word";
+    }
+
+    @Override
+    public List list() {
+        return Lists.newArrayList("zhangsan","list","laowang");
     }
 }

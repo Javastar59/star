66480e7b503f4d6f56f2034377b57a4f8caa499b
caoxing <sunstar59@163.com.cn>
2019-01-14 17:39:00 +0800

    update

--- application/src/main/java/com/star/application/controller/UserController.java
+++ application/src/main/java/com/star/application/controller/UserController.java
@@ -3,20 +3,12 @@ package com.star.application.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.star.common.api.UserApi;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class UserController {
 
-    @Reference(url = "dubbo://127.0.0.1:20881", version = "1.0.0")
+    @Reference(url = "dubbo://127.0.0.1:20881", version = "1.0.0", lazy = true)
     private UserApi userApi;
 
-    @ResponseBody
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String hello() {
-        return userApi.hello();
-    }
 }

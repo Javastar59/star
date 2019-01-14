328ec8b4aad3de7b96686bf68da3f3e050b4a682
caoxing <sunstar59@163.com.cn>
2019-01-14 22:48:20 +0800

    update

--- application/src/main/java/com/star/application/controller/UserController.java
+++ application/src/main/java/com/star/application/controller/UserController.java
@@ -3,6 +3,8 @@ package com.star.application.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.star.common.api.UserApi;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -11,4 +13,8 @@ public class UserController {
     @Reference(url = "dubbo://127.0.0.1:20881", version = "1.0.0", lazy = true)
     private UserApi userApi;
 
+    @RequestMapping(value = {"/admin"}, method = RequestMethod.GET)
+    public String goAdmin() {
+        return "admin/home";
+    }
 }
